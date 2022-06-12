9a3c5db8390334e8d75bbf4a615aa109242d72e4
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-06-12 10:43:55 +0100

    Funny and Pangrams Also

--- AlgorithmWarmup/Funny.cs
+++ AlgorithmWarmup/Funny.cs
@@ -48,65 +48,65 @@ namespace ProblemSolving
         }
     }
 
-//In this challenge, you will determine whether a string is funny or not.To determine
-//whether a string is funny, create a copy of the string in reverse e.g. .Iterating
-//through each string, compare the absolute difference in the ascii values of the
-//characters at positions 0 and 1, 1 and 2 and so on to the end. If the list of absolute
-//differences is the same for both strings, they are funny.
+    //In this challenge, you will determine whether a string is funny or not.To determine
+    //whether a string is funny, create a copy of the string in reverse e.g. .Iterating
+    //through each string, compare the absolute difference in the ascii values of the
+    //characters at positions 0 and 1, 1 and 2 and so on to the end. If the list of absolute
+    //differences is the same for both strings, they are funny.
 
-//Determine whether a give string is funny.If it is, return Funny,
-//otherwise return Not Funny.
+    //Determine whether a give string is funny.If it is, return Funny,
+    //otherwise return Not Funny.
 
-//Example
+    //Example
 
-//The ordinal values of the charcters are .  and the ordinals are.
-//The absolute differences of the adjacent elements for both strings are,
-//so the answer is Funny.
+    //The ordinal values of the charcters are .  and the ordinals are.
+    //The absolute differences of the adjacent elements for both strings are,
+    //so the answer is Funny.
 
-//Function Description
+    //Function Description
 
-//Complete the funnyString function in the editor below.
+    //Complete the funnyString function in the editor below.
 
-//funnyString has the following parameter(s):
+    //funnyString has the following parameter(s):
 
-//string s: a string to test
-//Returns
+    //string s: a string to test
+    //Returns
 
-//string: either Funny or Not Funny
-//Input Format
+    //string: either Funny or Not Funny
+    //Input Format
 
-//The first line contains an integer , the number of queries.
-//The next  lines each contain a string, .
+    //The first line contains an integer , the number of queries.
+    //The next  lines each contain a string, .
 
-//Constraints
+    //Constraints
 
-//Sample Input
+    //Sample Input
 
-//STDIN   Function
-//-----   --------
-//2       q = 2
-//acxz s = 'acxz'
-//bcxz s = 'bcxz'
-//Sample Output
+    //STDIN   Function
+    //-----   --------
+    //2       q = 2
+    //acxz s = 'acxz'
+    //bcxz s = 'bcxz'
+    //Sample Output
 
-//Funny
-//Not Funny
-//Explanation
+    //Funny
+    //Not Funny
+    //Explanation
 
-//Let be the reverse of.
+    //Let be the reverse of.
 
-//Test Case 0:
+    //Test Case 0:
 
-//,
-//Corresponding ASCII values of characters of the strings:
-// and
-//For both the strings the adjacent difference list is [2, 21, 2].
+    //,
+    //Corresponding ASCII values of characters of the strings:
+    // and
+    //For both the strings the adjacent difference list is [2, 21, 2].
 
-//Test Case 1:
+    //Test Case 1:
 
-//,
-//Corresponding ASCII values of characters of the strings:
-// and
-//The difference list for string  is [1, 21, 2] and for string  is [2, 21, 1].
+    //,
+    //Corresponding ASCII values of characters of the strings:
+    // and
+    //The difference list for string  is [1, 21, 2] and for string  is [2, 21, 1].
 
 }
